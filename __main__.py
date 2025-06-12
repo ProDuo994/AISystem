ffd6514393bd51dc166df73ad89e9a1596aba9a7
ProDuo994 <116141375+ProDuo994@users.noreply.github.com>
2025-06-12 10:16:14 +1000

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def submitRes(res):
+  if res:
+    print("Analysing Prompt")
+    for l in length(res):
+      pass
+  else:
+    print("It seems that you have not submited a prompt.")
+
+i = input("> ")
+submitRes(i)
