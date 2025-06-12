8017de9bd46ed57615a2797746dbaee260bafce6
ProDuo994 <116141375+ProDuo994@users.noreply.github.com>
2025-06-12 10:34:27 +1000

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,13 +1,28 @@
 import numpy as np
+import string
 
+data = {}
+chars = set(string.ascii_letters)
 
-def submitRes(res):
-  if res:
-    print("Analysing Prompt")
-    for l in length(res):
-      pass
-  else:
-    print("It seems that you have not submited a prompt.")
+def saveLearntData(data):
+  d = set(data)
+
+def convertPromptToData(prompt):
+    promptData = {} 
+    
+    for l in prompt:
+        if l in chars: 
+            promptData[l] = promptData.get(l, 0) + 1
+    return promptData 
+
+def submitQuery(query):
+    if query:
+        print("Analysing Query")
+        QData = convertPromptToData(query)
+        print("Processed Data:", QData)
+    else:
+        print("It seems that you have not submitted a prompt.")
+    saveLearntData(data)
 
 i = input("> ")
-submitRes(i)
+submitQuery(i)
