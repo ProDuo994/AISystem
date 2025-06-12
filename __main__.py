7597fa1ac19cfaa336ef83e6de0ef83237a993d2
ben speechley <xtros.gamer@gmail.com>
2025-06-12 12:06:05 +1000

    Changes

--- __main__.py
+++ __main__.py
@@ -4,33 +4,38 @@ import string
 data = {}
 chars = set(string.ascii_letters)
 
-class NueralNetwork:
-  def changeWeight(newWeight):
-    self.weights = newWeight
-  def __init__(self, weights):
-    self.weights = weights
+class NeuralNetwork:
+    def __init__(self, weights):
+        self.weights = weights
+
+    def changeWeight(self, newWeight):
+        self.weights = newWeight
 
 nn = NeuralNetwork(0.23)
 
-def saveLearntData(data):
-  d = set(data)
+def saveLearntData(newData):
+    global data
+    for k, v in newData.items():
+        data[k] = data.get(k, 0) + v
 
 def convertPromptToData(prompt):
-    promptData = {} 
-    
-    for l in prompt:
-        if l in chars: 
-            promptData[l] = promptData.get(l, 0) + 1
-    return promptData 
+    words = prompt.lower().split()  # simple tokenization
+    promptData = {}
+    for word in words:
+        promptData[word] = promptData.get(word, 0) + 1
+    return promptData
+
 
 def submitQuery(query):
     if query:
         print("Analysing Query")
         QData = convertPromptToData(query)
         print("Processed Data:", QData)
+        saveLearntData(QData)
     else:
         print("It seems that you have not submitted a prompt.")
-    saveLearntData(data)
 
+nn.changeWeight(0.45)
 i = input("> ")
 submitQuery(i)
+print("Accumulated Data:", data)
